127d41054257762a95eb9b2b8b08a9fc5c6c2536
Bruno Santos <bm-santos@outlook.com.br>
2021-02-25 01:47:37 -0300

    converted to next.js and increased countdown

---
+++ src/components/Profile/Profile.tsx
@@ -0,0 +1,16 @@
+import styles from '../../styles/components/Profile.module.css';
+
+export function Profile() {
+    return (
+        <div className={styles.profileContainer}>
+            <img src="https://github.com/bm-santos.png" alt="Bruno Santos" />
+            <div>
+                <strong>Bruno Santos</strong>
+                <p>
+                    <img src="icons/level.svg" alt=""/>
+                    Level 1
+                </p>
+            </div>
+        </div>
+    )
+}
