a63ab92fa330a74c1fc3ff22995600b28e775190
Bruno Santos <bm-santos@outlook.com.br>
2021-03-21 23:18:38 -0300

    feat: opens modal when user achieves next level

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Profile.module.css';
+import { ChallengeBox } from './ChallengeBox';
 
 export function Profile() {
+    const { level } = useContext(ChallengesContext)
     return (
         <div className={styles.profileContainer}>
             <img src="https://github.com/bm-santos.png" alt="Bruno Santos" />
@@ -8,7 +12,7 @@ export function Profile() {
                 <strong>Bruno Santos</strong>
                 <p>
                     <img src="icons/level.svg" alt="" />
-                    Level 1
+                    Level {level}
                 </p>
             </div>
         </div>
