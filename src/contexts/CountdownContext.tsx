9eb0a2fc4c2928505239a92fea89453d64065198
Bruno Santos <bm-santos@outlook.com.br>
2021-03-24 08:54:12 -0300

    fix: countdown set to 25min

--- src/contexts/CountdownContext.tsx
+++ src/contexts/CountdownContext.tsx
@@ -21,7 +21,7 @@ let countdownTimeout: NodeJS.Timeout;
 
 export function CountdownProvider({ children }: CountdownProviderProps) {
     const { startNewChallenge, resetChallenge } = useContext(ChallengesContext)
-    const initialTime = 10 * 60
+    const initialTime = 25 * 60
     const [time, setTime] = useState(initialTime)
     const [isActive, setIsActive] = useState(false)
     const [hasFinished, setHasFinished] = useState(false)
